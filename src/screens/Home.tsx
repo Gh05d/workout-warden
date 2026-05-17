3ec32f1ba20c370c82fe7f44ed72b971048424cb
Gh05d <pascla@mailbox.org>
2026-05-17 20:01:41 +0700

    Home: use DEMOTIVATIONAL_QUOTES and theme.colors; new picker API

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,76 +1,61 @@
+// src/screens/Home.tsx
 import React from 'react';
 import {Button, Image, StyleSheet, View} from 'react-native';
-import {
-  pick,
-  types,
-  errorCodes,
-  isErrorWithCode,
-} from '@react-native-documents/picker';
+import {pick, types, errorCodes, isErrorWithCode} from '@react-native-documents/picker';
 
-import {colors, demotivationalQuotes} from '../common/variables';
 import AppText from '../components/AppText';
+import {colors} from '../common/theme';
+import {DEMOTIVATIONAL_QUOTES} from '../common/quotes';
 import {exportDatabase, importDatabase} from '../common/databaseService';
+import type {BaseProps} from '../common/types';
 
 const Home: React.FC<BaseProps> = ({route}) => {
   const {puppy} = route?.params as {puppy: string};
-
   const [submitting, setSubmitting] = React.useState(false);
 
-  const quote =
-    demotivationalQuotes[
-      Math.floor(Math.random() * demotivationalQuotes.length)
-    ];
+  const quote = DEMOTIVATIONAL_QUOTES[Math.floor(Math.random() * DEMOTIVATIONAL_QUOTES.length)];
 
-  async function pickFile() {
+  async function pickFile(): Promise<string | undefined> {
     try {
       const [res] = await pick({type: [types.allFiles]});
-
       return res.uri;
     } catch (err) {
       if (isErrorWithCode(err) && err.code === errorCodes.OPERATION_CANCELED) {
         console.log('User canceled the picker');
-      } else throw err;
+        return;
+      }
+      throw err;
     }
   }
 
   return (
     <View style={styles.root}>
-      {!!puppy && (
-        <Image source={{uri: puppy}} style={{width: '100%', height: 300}} />
-      )}
+      {!!puppy && <Image source={{uri: puppy}} style={{width: '100%', height: 300}} />}
       <AppText style={{fontSize: 16}} italic>
         {quote}
       </AppText>
 
-      <Button
-        color={colors.primary}
-        title="Export data"
+      <Button color={colors.primary} title="Export data" disabled={submitting}
         onPress={async () => {
+          setSubmitting(true);
           try {
-            await setSubmitting(true);
             await exportDatabase();
-          } catch (_e) {
           } finally {
             setSubmitting(false);
           }
         }}
-        disabled={submitting}
       />
 
-      <Button
-        color={colors.primary}
-        title="Import data"
+      <Button color={colors.primary} title="Import data" disabled={submitting}
         onPress={async () => {
+          setSubmitting(true);
           try {
-            await setSubmitting(true);
             const data = await pickFile();
-            await importDatabase(data);
-          } catch (_e) {
+            if (data) await importDatabase(data);
           } finally {
             setSubmitting(false);
           }
         }}
-        disabled={submitting}
       />
     </View>
   );
