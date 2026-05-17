be9a59e95b769c8e88e47db8cda25dd164f407cd
Gh05d <pascla@mailbox.org>
2026-05-17 19:19:49 +0700

    Seed: barrel + integration test against validator

---
+++ src/seeds/index.ts
@@ -0,0 +1,6 @@
+import type {ExerciseSeed, PlanSeed} from '../common/types';
+import {EXERCISES as EXERCISES_LIST} from './exercises';
+import {SURF} from './plans/surf';
+
+export const EXERCISES: readonly ExerciseSeed[] = EXERCISES_LIST;
+export const PLANS: readonly PlanSeed[] = [SURF];
