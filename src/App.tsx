c9277a6a5d6afb5f28ddbcfd377692e6f84e1760
Gh05d <pascla@mailbox.org>
2026-05-17 19:39:46 +0700

    App.tsx: drop AsyncStorage TRAINING_TYPE coupling

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,31 @@
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-
+// src/App.tsx
 import React from 'react';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import SQLite from 'react-native-sqlite-storage';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Routes from './Routes';
 import SplashScreen from './components/Splash';
-import {initDB} from './common/databaseService';
 import Toast from './components/Toast';
-
-import {TRAINING_TYPE} from './common/variables';
+import {initDB} from './common/databaseService';
 
 SQLite.enablePromise(true);
 
-const App: React.FC<{}> = () => {
+const App: React.FC = () => {
   const [initiated, setInitiated] = React.useState(false);
   const [showSplash, setShowSplash] = React.useState(true);
   const [puppy, setPuppy] = React.useState('');
   const [error, setError] = React.useState<null | Error>(null);
-  const [type, setType] = React.useState('standard');
 
   React.useEffect(() => {
     (async function init() {
       try {
-        const [response, _, currentType] = await Promise.all([
+        const [response, _] = await Promise.all([
           fetch('https://dog.ceo/api/breeds/image/random'),
           initDB(),
-          AsyncStorage.getItem(TRAINING_TYPE),
         ]);
-
         const data = await response.json();
-        if (currentType) setType(currentType);
-        else await AsyncStorage.setItem(TRAINING_TYPE, type);
-
-        if (data?.status == 'success') setPuppy(data.message);
+        if (data?.status === 'success') setPuppy(data.message);
       } catch (err) {
         console.log(err);
         setError(err as Error);
@@ -57,7 +48,7 @@ const App: React.FC<{}> = () => {
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={{flex: 1}}>
-        <Routes puppy={puppy} type={type} />
+        <Routes puppy={puppy} />
         {error && (
           <Toast
             message={error.message}
